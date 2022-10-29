ace10f001b052a98585ce075ddfd783a29f573a2
abdulaziz <80a.tutorial@gmail.com>
2022-10-29 16:40:23 +0300

    test

---
+++ lib/BidsPage.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:final_project/ProfileNavigationDrawer.dart';
+import 'Navbars.dart';
+import 'HomePage.dart';
+
+class Bids extends StatefulWidget {
+  const Bids({super.key});
+
+  @override
+  State<Bids> createState() => _BidsState();
+}
+
+class _BidsState extends State<Bids> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey[300],
+        appBar: getAppBar(context),
+        drawerScrimColor: Colors.black38,
+        drawer: const ProfileNavigationDrawer(),
+        bottomNavigationBar: getButtonBar(context, setState),
+      ),
+    );
+  }
+}
--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'Product_options.dart';
-import './Firebase/FirebaseAction.dart';
-import './models/transaction.dart';
 import 'ProfileNavigationDrawer.dart';
 import 'ProductListPage.dart';
-import 'CustomSearchDelegate.dart';
+import 'Navbars.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -13,71 +10,14 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   @override
-  int _currentIndex = 0;
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-            backgroundColor: Colors.lightBlueAccent,
-            actions: [
-              // this is the search button
-              IconButton(
-                  onPressed: () {
-                    showSearch(
-                      context: context,
-                      delegate: CustomSearchDelegate(),
-                    );
-                  },
-                  icon: const Icon(Icons.search)),
-            ],
-            title: Padding(
-              padding: const EdgeInsets.only(left: 77),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Text(
-                    'ZAWD',
-                    style: TextStyle(
-                      fontFamily: 'Bellota',
-                      fontSize: 30,
-                      color: Colors.white,
-                    ),
-                  ),
-                  // Icon(
-                  //   Icons.notifications_none_outlined,
-                  //   color: Colors.white,
-                  // )
-                ],
-              ),
-            )),
-        drawerScrimColor: Colors.black38,
-        drawer: const ProfileNavigationDrawer(),
-        body: ProductListPage(),
-        bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: Colors.white,
-          currentIndex: _currentIndex,
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home_filled),
-              label: 'HOME',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.star),
-              label: 'AUCTION HOUSE',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.list_alt),
-              label: 'BIDS',
-            ),
-          ],
-          onTap: (index) {
-            setState(() {
-              _currentIndex = index;
-              // navigate to the corresponding page according to the index
-            });
-          },
-        ),
-      ),
+          appBar: getAppBar(context),
+          drawerScrimColor: Colors.black38,
+          drawer: const ProfileNavigationDrawer(),
+          body: ProductListPage(),
+          bottomNavigationBar: getButtonBar(context, setState)),
     );
   }
 }
