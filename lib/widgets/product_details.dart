ab0c08e41c3476c4635a4a9c2cf0b8da9b3c73a0
aaalmutairi <aaalmutairir@tahakom.com>
2022-10-29 21:29:56 +0300

    added SingleChildeScrollView to fix web view on browser

--- lib/widgets/product_details.dart
+++ lib/widgets/product_details.dart
@@ -40,118 +40,124 @@ class ProductDetails extends StatelessWidget {
       ),
       drawerScrimColor: Colors.black38,
       drawer: const ProfileNavigationDrawer(),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(8.0),
-            child: Image.network(
-              'https://thumbs.dreamstime.com/b/chisinau-moldova-mar%D1%81h-mercedes-benz-brabus-g-v-cv-g-amg-w-mercedes-benz-brabus-g-176915907.jpg',
-              width: MediaQuery.of(context).size.width,
-              fit: BoxFit.cover,
-            ),
-          ),
-          Container(
-            height: 300,
-            margin: EdgeInsets.all(8.0),
-            child: Card(
-              elevation: 5,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(8.0),
+                child: Image.network(
+                  'https://thumbs.dreamstime.com/b/chisinau-moldova-mar%D1%81h-mercedes-benz-brabus-g-v-cv-g-amg-w-mercedes-benz-brabus-g-176915907.jpg',
+                  width: MediaQuery.of(context).size.width,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Container(
+                height: 300,
+                margin: EdgeInsets.all(8.0),
+                child: Card(
+                  elevation: 5,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: <Widget>[
-                      Text(
-                        _title,
-                        style: TextStyle(
-                          color: Colors.blue,
-                          fontSize: 40,
-                          fontWeight: FontWeight.bold,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            _title,
+                            style: TextStyle(
+                              color: Colors.blue,
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Container(
+                            // padding: EdgeInsets.all(2),
+                            child: Row(children: <Widget>[
+                              Text(
+                                _onwer,
+                                style:
+                                    TextStyle(color: Colors.grey, fontSize: 25),
+                              ),
+                              RatingBar.builder(
+                                itemSize: 20.0,
+                                maxRating: 5,
+                                ignoreGestures: true,
+                                initialRating: 3.5,
+                                minRating: 1,
+                                direction: Axis.horizontal,
+                                allowHalfRating: true,
+                                itemCount: 5,
+                                itemPadding:
+                                    EdgeInsets.symmetric(horizontal: 4.0),
+                                itemBuilder: (context, _) => Icon(
+                                  Icons.star,
+                                  color: Colors.blue,
+                                ),
+                                onRatingUpdate: (rating) {
+                                  print(rating);
+                                },
+                              ),
+                            ]),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Container(
+                              padding: EdgeInsets.all(5),
+                              decoration: BoxDecoration(
+                                  color: Colors.amber[700],
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(4))),
+                              child: Text(
+                                _price.toStringAsFixed(1) + ' ' + _currency,
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 15,
+                                    color: Colors.white),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(8.0),
+                              child: Text('timer'),
+                            )
+                          ],
                         ),
                       ),
                       Container(
-                        // padding: EdgeInsets.all(2),
-                        child: Row(children: <Widget>[
-                          Text(
-                            _onwer,
-                            style: TextStyle(color: Colors.grey, fontSize: 25),
-                          ),
-                          RatingBar.builder(
-                            itemSize: 20.0,
-                            maxRating: 5,
-                            ignoreGestures: true,
-                            initialRating: 3.5,
-                            minRating: 1,
-                            direction: Axis.horizontal,
-                            allowHalfRating: true,
-                            itemCount: 5,
-                            itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
-                            itemBuilder: (context, _) => Icon(
-                              Icons.star,
-                              color: Colors.blue,
-                            ),
-                            onRatingUpdate: (rating) {
-                              print(rating);
-                            },
-                          ),
-                        ]),
-                      ),
-                    ],
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Container(
-                          padding: EdgeInsets.all(5),
-                          decoration: BoxDecoration(
-                              color: Colors.amber[700],
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(4))),
+                        padding: EdgeInsets.all(8.0),
+                        child: Flexible(
                           child: Text(
-                            _price.toStringAsFixed(1) + ' ' + _currency,
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 15,
-                                color: Colors.white),
+                            _description,
+                            style: TextStyle(fontSize: 18),
                           ),
                         ),
-                        Container(
-                          padding: EdgeInsets.all(8.0),
-                          child: Text('timer'),
-                        )
-                      ],
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(8.0),
-                    child: Flexible(
-                      child: Text(
-                        _description,
-                        style: TextStyle(fontSize: 18),
                       ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Text(
-                      _biddersLable,
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text(
+                          _biddersLable,
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                       ),
-                    ),
+                      Container(
+                          // height: 200,
+                          )
+                    ],
                   ),
-                  Container(
-                      // height: 200,
-                      )
-                ],
+                ),
               ),
-            ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
